3ccfe7cb5b0c4a84dea682e13a2fb61a03b55187
mattiaoui <54487132+mattiaoui@users.noreply.github.com>
2020-09-01 12:44:38 +0200

    Initial commit

---
+++ ScriptUbuntu.sh
@@ -0,0 +1,5 @@
+ansible-playbook /AnsibleUbuntuServer/deploy_all/base_config.yaml -i /AnsibleUbuntuServer/hostgroup/hosts
+wait
+ansible-playbook /AnsibleUbuntuServer/deploy_ubuntu/new_server.yaml -i /AnsibleUbuntuServer/hostgroup/hosts
+wait
+ansible-playbook /AnsibleUbuntuServer/deploy_ubuntu/telegraf.yaml -i /AnsibleUbuntuServer/hostgroup/hosts
