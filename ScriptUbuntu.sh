90177c3eb35bcd9c03185ffa2991bd3fb3811f16
mattiaoui <54487132+mattiaoui@users.noreply.github.com>
2020-09-02 15:04:04 +0200

    2.0

--- ScriptUbuntu.sh
+++ ScriptUbuntu.sh
@@ -4,4 +4,6 @@ ansible-playbook /AnsibleUbuntuServer/deploy_ubuntu/new_server.yaml -i /AnsibleU
 wait
 ansible-playbook /AnsibleUbuntuServer/deploy_ubuntu/telegraf.yaml -i /AnsibleUbuntuServer/hostgroup/hosts
 wait
+ansible-playbook /AnsibleUbuntuServer/deploy_ubuntu/docker.yaml -i /AnsibleUbuntuServer/hostgroup/hosts
+wait
 ansible-playbook /AnsibleUbuntuServer/deploy_ubuntu/reboot.yaml -i /AnsibleUbuntuServer/hostgroup/hosts
